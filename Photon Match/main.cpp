39bb21cad30a87081aafc2546b537caf26fc4b6e
Robert Jay Myers <myers.robertj@gmail.com>
2019-10-27 01:06:29 -0400

    Added program icon.

--- Photon Match/main.cpp
+++ Photon Match/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	a.setWindowIcon(QIcon(":/PhotonMatch/Icon/photon-match-program-icon.ico"));
 	PhotonMatch w;
 	w.show();
 	return a.exec();
